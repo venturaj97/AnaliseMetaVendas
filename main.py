269f2ab4576e2ed7a1e4cc2141a8c1fe1f027250
João Victor Ventura Mendes <joaoventura97@outlook.com>
2023-10-24 15:28:54 -0300

    Primeiro commit

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import pandas as pd
 from twilio.rest import Client
 #passo a passo
 
-account_sid = "AC9b58d3e8f621de42ab588c6d9e78b1cb"
-auth_token = "07cdf2816ee91e21b71fae8ecc833726"
+account_sid = "SEU ID TWILIO"
+auth_token = "SEU TOKEN TWILIO"
 client = Client(account_sid, auth_token)
 
 #abrir os 6 arquivos em excel
@@ -18,9 +18,9 @@ for mes in lista_meses:   # para cada arquivo:
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
         print(f'No mês de {mes} a meta foi batida. Vendedor: {vendedor}, Vendas: {vendas}.')
         message = client.messages.create(
-            body=f'No mês de {mes} a meta foi batida. Vendedor: {vendedor}, Vendas: {vendas}.',
-            from_='+19207538040',
-            to='+5521980115136'
+            body=f"mês de {mes} a meta foi batida. Vendedor: {vendedor}, Vendas: {vendas}.",
+            from_='SEU NÚMERO GERADO PELO TWILIO',
+            to='NÚMERO DE TELEFONE NO QUAL VOCE QUER RECEBER O SMS COM CODIGO DO PAIS DDD DO ESTADO E NUMERO'
         )
         print(message.sid)
 
